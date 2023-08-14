b1d9c78399706d23e22ff5ac2f3a4a9247a5cf3a
Geoffrey <gdhnz@users.noreply.github.com>
2023-08-15 10:31:45 +1200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from markupsafe import escape
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+@application.route("/<name>")
+def hello_name(name)
+    return f'Hello {escape(username)}'
 
 if __name__ == "__main__":
     application.run()
